0237b49d3ed3fd3bcdb03bc06f4675d90f246487
GabrielaAmbos <gabrielaambos15@gmail.com>
2020-10-18 19:13:12 -0300

    Adicionando os testes de aceitacao e contrato

--- src/test/java/br/com/restassuredapitesting/tests/booking/tests/GetBookingTest.java
+++ src/test/java/br/com/restassuredapitesting/tests/booking/tests/GetBookingTest.java
@@ -1,24 +1,51 @@
 package br.com.restassuredapitesting.tests.booking.tests;
 
+import br.com.restassuredapitesting.runners.Acceptance;
+import br.com.restassuredapitesting.runners.Contract;
 import br.com.restassuredapitesting.tests.base.tests.BaseTest;
 import br.com.restassuredapitesting.tests.booking.requests.GetBookingRequest;
+import br.com.restassuredapitesting.utils.Utils;
+import io.qameta.allure.Feature;
 import io.qameta.allure.Severity;
 import io.qameta.allure.SeverityLevel;
 import io.qameta.allure.junit4.DisplayName;
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
 
+import java.io.File;
+
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchema;
 import static org.hamcrest.Matchers.greaterThan;
 
+@Feature("Reservas")
 public class GetBookingTest extends BaseTest {
 
     GetBookingRequest getBookingRequest = new GetBookingRequest();
 
     @Test
     @Severity(SeverityLevel.NORMAL)
+    @Category(Acceptance.class)
     @DisplayName("Listar ID's das reservas")
     public void validarIDsDasReservas() throws Exception{
         getBookingRequest.allBookings().then()
                 .statusCode(200)
                 .body("size()",greaterThan(0));
+    }
+
+    @Test
+    @Severity(SeverityLevel.BLOCKER)
+    @Category(Contract.class)
+    @DisplayName("Garantir o contrato de retorno da lista de reservas")
+    public void garantirContratoListaReservas() throws Exception {
+        getBookingRequest.allBookings().then()
+                .statusCode(200)
+                .assertThat()
+                .body(
+                        matchesJsonSchema(
+                            new File(
+                                Utils.getContractsBasePath("booking", "bookings")
+                            )
+                        )
+                );
     }
 }
--- src/test/java/br/com/restassuredapitesting/utils/Utils.java
+++ src/test/java/br/com/restassuredapitesting/utils/Utils.java
@@ -1,14 +1,32 @@
 package br.com.restassuredapitesting.utils;
 
+import org.json.simple.JSONObject;
+
 public class Utils {
 
 
-    public static  String getContractsBasePath(String pack, String contract) {
+    public static String getContractsBasePath(String pack, String contract) {
         return System.getProperty("user.dir")
                 + "/src/test/java/br/com/restassuredapitesting/tests/"
                 + pack
                 + "/contracts/"
                 + contract
                 + ".json";
+    }
+
+    public static JSONObject validPayloadBooking() {
+        JSONObject payload = new JSONObject();
+        JSONObject bookingDates = new JSONObject();
+        bookingDates.put("checkin", "2018-01-01");
+        bookingDates.put("checkout", "2019-01-01");
+
+        payload.put("firstname", "Jim");
+        payload.put("lastname", "Brown");
+        payload.put("totalprice", 111);
+        payload.put("depositpaid", true);
+        payload.put("bookingDates", bookingDates);
+        payload.put("additionalneeds", "Breakfast");
+
+        return payload;
     }
 }
